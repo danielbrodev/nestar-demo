686b808e3add860360fd9f22e075b23b2f520bbb
danielbrodev <xidirovdiyorbek6@gmail.com>
2026-04-30 20:46:04 +0900

    feat: integration community and property pages and header footer via layouts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,28 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  console.log("Document Rendering");
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar-demo" />
+        <meta name="robots" content="index, follow" />
+        <link rel="icon" type="img/png" href="/img/logo/favicon.svg" />
+        {/* SEO OPTIMIZATION */}
+        <meta
+          name="keyword"
+          content={"Nestar, Nestar uz, devex mern , mern nestjs fullstack"}
+        />
+        <meta
+          name="description"
+          content={
+            "Koreyaing har nuqtasidan kochmas mulklarni sotib oling , soting. |" +
+            "shuning koreyschasi . |" +
+            "shuning ruschasi"
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
@@ -11,3 +30,6 @@ export default function Document() {
     </Html>
   );
 }
+
+// Toliq server side rendering da ishga tushadi. har bir request da ishga tushadi  hamda Search Engine Optimization uchun ishlatiladi
+// Meta datalar uchun ishlatiladi
---
+++ pages/property/detail.tsx
@@ -0,0 +1,11 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { Container } from "@mui/material";
+
+const PropertyDetailPage = () => {
+  return (
+    <>
+      <Container>Properties Detail</Container>
+    </>
+  );
+};
+export default withLayoutBasic(PropertyDetailPage);
